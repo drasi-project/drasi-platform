4bf0fbb6825ed89c759d5905505afee21f2ba5be
ruokun-niu <ruokunniu@microsoft.com>
2024-10-15 13:20:02 -0700

    init

--- dev-tools/smoke-tests/smoke-test.sh
+++ dev-tools/smoke-tests/smoke-test.sh
@@ -10,7 +10,7 @@ fi
 echo Setting up Postgres...
 
 # Deploy Postgres   TODO: Update the URL
-kubectl apply -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-postgres.yaml -n default
+kubectl apply -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-postgres.yaml -n default
 kubectl wait --for=condition=ready --timeout=60s pod -l app=postgres -n default
 
 
@@ -22,17 +22,17 @@ drasi namespace set $namespace   # Set the namespace
 
 # Apply source
 echo Applying a Source with the name 'smoke-test'
-drasi apply -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-source.yaml
+drasi apply -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-source.yaml
 drasi wait source smoke-test -t 120
 
 # Apply continuous query 
 echo Applying a ContinuousQuery with the name 'smoke-query'
-drasi apply -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-query.yaml
+drasi apply -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-query.yaml
 sleep 5
 
 # Apply Reaction
 echo Applying a Reaction with the name 'smoke-result-reaction'
-drasi apply -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-reaction.yaml
+drasi apply -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-reaction.yaml
 
 drasi wait reaction smoke-result-reaction -t 120
 
@@ -68,16 +68,16 @@ if [ "$final_parsed_output" == "$expected_output" ]; then
     echo "Smoke test passed!"
 
     echo "cleaning up resources..."
-    kubectl delete -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-postgres.yaml -n default
+    kubectl delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-postgres.yaml -n default
 
-    drasi delete -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-source.yaml
-    drasi delete -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-query.yaml
-    drasi delete -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-reaction.yaml
+    drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-source.yaml
+    drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-query.yaml
+    drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-reaction.yaml
 
 
 else
     echo "Smoke test failed"
 
-    echo "Resources are not deleted. If you wish to clean up everything, run 'curl -s https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/smoke-tests/cleanup-smoke-test.sh | bash'"
+    echo "Resources are not deleted. If you wish to clean up everything, run 'curl -s https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/cleanup-smoke-test.sh | bash'"
     exit 1
 fi
