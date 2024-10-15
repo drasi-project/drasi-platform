f1e923a5899850ebd8a80629adde61411ece6e3e
ruokun-niu <ruokunniu@microsoft.com>
2024-10-15 14:15:10 -0700

    url update

--- dev-tools/smoke-tests/cleanup-smoke-test.sh
+++ dev-tools/smoke-tests/cleanup-smoke-test.sh
@@ -6,8 +6,8 @@ fi
 
 
 drasi namespace set $namespace
-kubectl delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test/dev-tools/smoke-tests/resources/smoke-test-postgres.yaml -n default
+kubectl delete -f https://raw.githubusercontent.com/main/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-postgres.yaml -n default
 
-drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test/dev-tools/smoke-tests/resources/smoke-test-source.yaml
-drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test/dev-tools/smoke-tests/resources/smoke-test-query.yaml
-drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test/dev-tools/smoke-tests/resources/smoke-test-reaction.yaml
+drasi delete -f https://raw.githubusercontent.com/main/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-source.yaml
+drasi delete -f https://raw.githubusercontent.com/main/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-query.yaml
+drasi delete -f https://raw.githubusercontent.com/main/drasi-platform/main/dev-tools/smoke-tests/resources/smoke-test-reaction.yaml
