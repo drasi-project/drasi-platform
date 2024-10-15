81c9723c95379407797272834648e3e57458d625
ruokun-niu <ruokunniu@microsoft.com>
2024-10-15 13:28:59 -0700

    powershell

--- dev-tools/smoke-tests/smoke-test.ps1
+++ dev-tools/smoke-tests/smoke-test.ps1
@@ -35,3 +35,72 @@ Write-Host "Applying a Reaction with the name 'smoke-result-reaction'"
 drasi apply -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test/dev-tools/smoke-tests/resources/smoke-test-reaction.yaml
 
 drasi wait reaction smoke-result-reaction -t 120
+
+# Run kubectl command to execute the curl pod and fetch the result
+$initial_output = kubectl run curl-pod --image=curlimages/curl -n $namespace --restart=Never --rm --attach -q -- sh -c 'curl -s http://smoke-result-reaction-gateway:8080/smoke-query/all' 2>$null
+
+# Extract the portion of the output that matches the pattern [.*]
+$initial_parsed_output = $initial_output -match '\[.*\]' | Out-Null
+$initial_parsed_output = $Matches[0]
+
+# Print the parsed output
+Write-Host "Initial output: $initial_parsed_output"
+
+# Retrieve the name of the postgres pod
+$postgres_pod = kubectl get pods -l app=postgres -o jsonpath="{.items[0].metadata.name}" -n default
+
+# Log the first insertion
+Write-Host "Inserting the following entries into the database: {'Id': 4, 'Name': 'Item 4', 'Category': 'B'}"
+Write-Host "postgres pod: $postgres_pod"
+
+# Execute the first SQL insert command inside the postgres pod
+kubectl exec $postgres_pod -n default -- psql -U postgres -d smokedb -c "INSERT INTO public.\"Item\" VALUES (4, 'Item 4', 'B')" 2>$null
+
+Start-Sleep -Seconds 5
+
+Write-Host "Inserting the following entries into the database: {'Id': 5, 'Name': 'Item 5', 'Category': 'A'}"
+
+kubectl exec $postgres_pod -n default -- psql -U postgres -d smokedb -c "INSERT INTO public.\"Item\" VALUES (5, 'Item 5', 'A')" 2>$null
+
+
+Write-Host "Retrieving the current result from the debug reaction"
+
+
+Start-Sleep -Seconds 20
+
+
+$final_output = kubectl run curl-pod --image=curlimages/curl -n $namespace --restart=Never --rm --attach -q -- sh -c 'curl -s http://smoke-result-reaction-gateway:8080/smoke-query/all' 2>$null
+
+# Extract the portion of the output that matches the pattern [.*]
+$final_parsed_output = $final_output -match '\[.*\]' | Out-Null
+$final_parsed_output = $Matches[0]
+
+Write-Host "Final output: $final_parsed_output"
+
+$expected_output = '[{"Category":"A","Id":1,"Name":"Item 1"},{"Category":"A","Id":3,"Name":"Item 3"},{"Category":"A","Id":5,"Name":"Item 5"}]'
+
+Write-Host "Expected output after the insertion: $expected_output"
+
+if ($final_parsed_output -eq $expected_output) {
+    Write-Host "Smoke test passed!"
+
+    # Cleaning up resources
+    Write-Host "Cleaning up resources..."
+    kubectl delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-postgres.yaml -n default
+
+    drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-source.yaml
+    drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-query.yaml
+    drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-reaction.yaml
+} else {
+    Write-Host "Smoke test failed"
+
+    Write-Host "Resources are not deleted. If you wish to clean up everything, run the following commands:"
+
+    Write-Host "kubectl delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-postgres.yaml -n default"
+    Write-Host "drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-source.yaml"
+    Write-Host "drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-query.yaml"
+    Write-Host "drasi delete -f https://raw.githubusercontent.com/ruokun-niu/drasi-platform/smoke-test-test/dev-tools/smoke-tests/resources/smoke-test-reaction.yaml"
+    
+    
+    exit 1
+}
