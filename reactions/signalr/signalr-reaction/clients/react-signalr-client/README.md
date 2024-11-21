44b44ef0c228e6594ca1d025ddf3332bf823bae5
Daniel Gerlag <daniel@gerlag.ca>
2024-11-20 20:48:37 -0800

    react client

---
+++ reactions/signalr/signalr-reaction/clients/react-signalr-client/README.md
@@ -0,0 +1,1 @@
+# @drasi/react-signalr-client
---
+++ reactions/signalr/signalr-reaction/clients/react-signalr-client/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+interface Props {
+  text: string
+}
+
+export const ExampleComponent = ({ text }: Props) => {
+  return <div>Example Component4: {text}</div>
+}
