be24fd7156756db53aa0e626ca15fa721c0b2db5
Ruokun (Tommy) Niu <ruokunniu@microsoft.com>
2025-03-20 10:38:01 -0700

    Review debug reaction (#155)
    * wip
    
    * nit
    
    * react
    
    * wip
    
    * basic working
    
    * event stream adn query status
    
    * event stream done and added basic css
    
    * updated file structure
    
    * Revert "updated file structure"
    
    This reverts commit 5f3d79374bf442d540694fbc2b6b4b450ee4d0b8.
    
    * updated file structure
    
    * sdk working
    
    * final
    
    * updated reaction provider
    
    * nit and license
    
    * updated makefile and github workflows
    
    * css for sidebar and eventstream
    
    * updated webapplication in debug reaction and SDK
    
    * logging update
    
    * updated GET for the queries
    
    * use resutlviewclient from the sdk
    
    * query container id update
    
    * updated to use strongly typed models
    
    * interface for broadcaster
    
    * nit
    
    * remove comments
    
    * updated source rust dockerfiles
    
    * dotnet format
    
    * moved classes to separate files
    
    * removed unused ExecuteAsync
    
    * moved the stream state to the frontend
    
    * moved queryreslt state to frontend;
    
    * updated the mechanism for clearing the query reselt
    
    * created websocketcontroller
    
    * combined frontend and backend into one container
    
    * updated the logic for update and delete in the frontend
    
    * updated serialization
    
    * updated image ame
    
    * nit
    
    * updated build-test
    
    * removed the unused list for raw events in the backend
    
    * update the logic for fetching the query result
    
    * removed unused dict
    
    * k8s source sdk version

---
+++ reactions/platform/debug-reaction/Services/WebSocketService.cs
@@ -0,0 +1,82 @@
+// Copyright 2025 The Drasi Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Net.WebSockets;
+using System.Collections.Concurrent;
+using System.Text;
+using Drasi.Reactions.Debug.Server.Models;
+using Drasi.Reactions.Debug.Server.Services;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+
+public class WebSocketService : IChangeBroadcaster
+{
+	private readonly ConcurrentDictionary<string, WebSocket> _connections = new();
+
+	private readonly ILogger<WebSocketService> _logger;
+
+	public WebSocketService(ILogger<WebSocketService> logger)
+	{
+		_logger = logger;
+	}
+	public void AddConnection(string socketName, WebSocket webSocket)
+	{
+		_connections[socketName] = webSocket;
+		_logger.LogInformation($"WebSocket registered for {socketName}");
+	}
+
+	public async Task BroadcastToQueryId(string socketName, QueryResult message)
+	{
+		_logger.LogInformation($"[{DateTime.UtcNow}] Broadcasting message to query: {socketName}");
+		if (_connections.ContainsKey(socketName))
+		{
+			var jsonMessage = JsonSerializer.Serialize(message);
+			var webSocket = _connections[socketName];
+			if (webSocket.State == WebSocketState.Open)
+			{
+				try
+				{
+					var buffer = Encoding.UTF8.GetBytes(jsonMessage);
+					await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+				}
+				catch (Exception ex)
+				{
+					_logger.LogError($"[{DateTime.UtcNow}] Error sending message to WebSocket: {ex.Message}");
+				}
+			}
+		}
+	}
+
+	public async Task BroadcastToStream(string socketName, JsonElement message)
+	{
+		_logger.LogInformation($"[{DateTime.UtcNow}] Broadcasting message to stream");
+		if (_connections.ContainsKey(socketName))
+		{
+			var jsonMessage = JsonSerializer.Serialize(message);
+			var webSocket = _connections[socketName];
+			if (webSocket.State == WebSocketState.Open)
+			{
+				try
+				{
+					var buffer = Encoding.UTF8.GetBytes(jsonMessage);
+					await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+				}
+				catch (Exception ex)
+				{
+					_logger.LogError($"[{DateTime.UtcNow}] Error sending message to WebSocket: {ex.Message}");
+				}
+			}
+		}
+	}
+}
