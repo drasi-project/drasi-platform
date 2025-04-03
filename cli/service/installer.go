96bc5066577f70fc0464683c9acceb4b3632d21f
ruokun-niu <ruokunniu@microsoft.com>
2025-04-03 14:36:14 -0700

    removed zipkin; updated tracing and metrics

--- cli/service/installer.go
+++ cli/service/installer.go
@@ -180,60 +180,49 @@ func (t *Installer) installInfrastructure(output output.TaskOutput, observabilit
 		return err
 	}
 
-	if observabilityLevel == "tracing" || observabilityLevel == "full" {
-		if raw, err = resources.ReadFile("resources/observability/tracing.yaml"); err != nil {
+	// Grafana is installed if the observability level is not none
+	if observabilityLevel != "none" {
+		var fileName string
+		if observabilityLevel == "tracing" {
+			fileName = "tracing.yaml"
+		} else if observabilityLevel == "metrics" {
+			fileName = "metrics.yaml"
+		} else if observabilityLevel == "full" {
+			fileName = "full-observability.yaml"
+		}
+
+		if raw, err = resources.ReadFile("resources/observability/" + fileName); err != nil {
 			return err
 		}
 
 		if observabilityManifests, err = readK8sManifests(raw); err != nil {
 			return err
 		}
+		output.AddTask("Observability", "Deploying observability infrastructure...")
 
-		output.AddTask("Tracing", "Deploying tracing infrastructure...")
 		if err = t.applyManifests(observabilityManifests); err != nil {
+			output.FailTask("Observability", "Error deploying observability infrastructure")
 			return err
 		}
 
-		subOutput = output.GetChildren("Tracing")
-
-		if err = t.waitForDeployment("app=zipkin", subOutput); err != nil {
-			return err
+		subOutput = output.GetChildren("Observability")
+		if observabilityLevel == "tracing" || observabilityLevel == "full" {
+			if err = t.waitForDeployment("app=tempo", subOutput); err != nil {
+				return err
+			}
 		}
-
-		output.SucceedTask("Tracing", "Tracing infrastructure deployed")
-	}
-	if observabilityLevel == "metrics" || observabilityLevel == "full" {
-		if raw, err = resources.ReadFile("resources/observability/metrics.yaml"); err != nil {
-			return err
-		}
-
-		if observabilityManifests, err = readK8sManifests(raw); err != nil {
-			return err
-		}
-
-		output.AddTask("Metrics", "Deploying metrics infrastructure...")
-		if err = t.applyManifests(observabilityManifests); err != nil {
-			output.FailTask("Metrics", "Error deploying metrics infrastructure")
-			return err
-		}
-
-		subOutput = output.GetChildren("Metrics")
-
-		if err = t.waitForDeployment("app=tempo", subOutput); err != nil {
-			return err
-		}
-
-		if err = t.waitForDeployment("app=prometheus", subOutput); err != nil {
-			return err
+		if observabilityLevel == "metrics" || observabilityLevel == "full" {
+			if err = t.waitForDeployment("app=prometheus", subOutput); err != nil {
+				return err
+			}
 		}
 
 		if err = t.waitForDeployment("app=grafana", subOutput); err != nil {
 			return err
 		}
+		output.SucceedTask("Observability", "Observability infrastructure deployed")
 
-		output.SucceedTask("Metrics", "Metrics infrastructure deployed")
 	}
-
 	output.SucceedTask("Infrastructure", "Infrastructure deployed")
 
 	return nil
