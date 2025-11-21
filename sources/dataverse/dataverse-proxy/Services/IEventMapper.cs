550627abf4d856896a69349e62ec7781383b5d36
Ruokun (Tommy) Niu <ruokunniu@microsoft.com>
2025-11-20 19:34:47 -0800

    Update Dataverse source (#336)
    Signed-off-by: ruokun-niu <ruokunniu@gmail.com>

--- sources/dataverse/dataverse-proxy/Services/IEventMapper.cs
+++ sources/dataverse/dataverse-proxy/Services/IEventMapper.cs
@@ -1,4 +1,4 @@
-// Copyright 2024 The Drasi Authors.
+// Copyright 2025 The Drasi Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,12 +13,12 @@
 // limitations under the License.
 
 using Microsoft.Xrm.Sdk;
-using Proxy.Models;
+using Drasi.Source.SDK.Models;
 
-namespace Proxy.Services
+namespace DataverseProxy.Services
 {
     interface IEventMapper
     {
-        Task<VertexState> MapEventAsync(IChangedItem rawEvent);
+        Task<SourceElement> MapEventAsync(IChangedItem rawEvent);
     }
 }
