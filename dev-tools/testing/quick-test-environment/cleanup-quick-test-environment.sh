6b4d10c5db34d4b4b919443dcb7fd4a120a5300e
ffelixg <142172984+ffelixg@users.noreply.github.com>
2026-02-06 19:50:40 +0100

    Fix broken URLs in quick-test cleanup scripts (#382)
    Signed-off-by: ffelixg <142172984+ffelixg@users.noreply.github.com>

--- dev-tools/testing/quick-test-environment/cleanup-quick-test-environment.sh
+++ dev-tools/testing/quick-test-environment/cleanup-quick-test-environment.sh
@@ -6,9 +6,9 @@ fi
 
 
 drasi namespace set $namespace
-kubectl delete -f https://raw.githubusercontent.com/main/drasi-platform/main/dev-tools/testing/quick-test-environment/resources/quick-test-postgres.yaml -n default
+kubectl delete -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/testing/quick-test-environment/resources/quick-test-postgres.yaml -n default
 
-drasi delete -f https://raw.githubusercontent.com/main/drasi-platform/main/dev-tools/testing/quick-test-environment/resources/quick-test-reaction.yaml
-drasi delete -f https://raw.githubusercontent.com/main/drasi-platform/main/dev-tools/testing/quick-test-environment/resources/quick-test-query.yaml
-drasi delete -f https://raw.githubusercontent.com/main/drasi-platform/main/dev-tools/testing/quick-test-environment/resources/quick-test-source.yaml
+drasi delete -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/testing/quick-test-environment/resources/quick-test-reaction.yaml
+drasi delete -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/testing/quick-test-environment/resources/quick-test-query.yaml
+drasi delete -f https://raw.githubusercontent.com/drasi-project/drasi-platform/main/dev-tools/testing/quick-test-environment/resources/quick-test-source.yaml
 
